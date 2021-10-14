ddd0bdcbc5e831a073f73e57dd04daefbd3b5f30
CHOI <godgod153@gmail.com>
2021-10-14 20:19:51 +0900

    livedata,viewmodel,databinding을 이용했다

--- app/src/main/java/com/cookandroid/livedata_viewmodel_toy/MainActivity.kt
+++ app/src/main/java/com/cookandroid/livedata_viewmodel_toy/MainActivity.kt
@@ -9,10 +9,11 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.cookandroid.livedata_viewmodel_toy.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     companion object{
         const val TAG:String = "로그"
@@ -22,28 +23,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         mainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-        mainActivityViewModel.currentValue.observe(this, Observer {
-            Log.d(TAG,"MainActivity - mainActivityViewModel - currentValue 라이브 데이터 값 변경 : $it")
-            findViewById<TextView>(R.id.tx).text=it.toString()
-        })
-        findViewById<Button>(R.id.plus_btn).setOnClickListener(this)
-        findViewById<Button>(R.id.minus_btn).setOnClickListener(this)
-
-    }
-
-    override fun onClick(v: View?) {
-        val userInput = findViewById<EditText>(R.id.edt).text.toString().toInt()
-
-        when(v){
-            findViewById<Button>(R.id.plus_btn)->{
-                mainActivityViewModel.updateValue(actionType = ActionType.PLUS,userInput)
-            }
-            findViewById<Button>(R.id.minus_btn)->{
-                mainActivityViewModel.updateValue(actionType = ActionType.MINUS,userInput)
-            }
-        }
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        binding.viewModel=mainActivityViewModel
+        binding.lifecycleOwner=this
     }
 }
--- app/src/main/java/com/cookandroid/livedata_viewmodel_toy/MainActivityViewModel.kt
+++ app/src/main/java/com/cookandroid/livedata_viewmodel_toy/MainActivityViewModel.kt
@@ -1,6 +1,10 @@
 package com.cookandroid.livedata_viewmodel_toy
 
+import android.app.Application
 import android.util.Log
+import android.widget.Toast
+import androidx.databinding.ObservableField
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -8,31 +12,39 @@ enum class ActionType{
     PLUS,MINUS
 }
 
-class MainActivityViewModel : ViewModel(){
+class MainActivityViewModel(application: Application) : AndroidViewModel(application){
+
 
     companion object{
         const val TAG:String = "로그"
     }
+    val mApplication = application
 
-    private val _currentValue=MutableLiveData<Int>()
+    private val _currentValue=MutableLiveData<String>()
+    private val _inputValue=MutableLiveData<String>()
 
-    val currentValue:MutableLiveData<Int>
+    val currentValue:MutableLiveData<String>
         get()=_currentValue
+
+    val inputValue:MutableLiveData<String>
+        get()=_inputValue
 
 
     //currentValue초기값 설정
     init {
         Log.d(TAG,"MainActivityViewModel - 생성자 호출")
-        _currentValue.value=0
+        _currentValue.value="0"
+        _inputValue.value="0"
     }
 
-    fun updateValue(actionType: ActionType,input:Int){
-        when(actionType){
-            ActionType.PLUS->
-                _currentValue.value= _currentValue.value?.plus(input)
-            ActionType.MINUS->
-                _currentValue.value=_currentValue.value?.minus(input)
-        }
+    //더하기 버튼이 눌렸을 때 실행
+    fun onClickPlusButton(){
+        _currentValue.value= _currentValue.value?.toInt()?.plus(_inputValue.value!!.toInt()).toString()
     }
 
+    //빼기 버튼이 눌렸을 때 실행
+    fun onClickMinusButton() {
+        _currentValue.value =
+            _currentValue.value?.toInt()?.minus(_inputValue.value!!.toInt()).toString()
+    }
 }
